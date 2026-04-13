827ca8b33b80a2616f65a8c03f7817d37154e8ce
Benjamin Lobos Lertpunyaroj <blobosle@purdue.edu>
2026-04-13 18:37:10 -0400

    added telescope port

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,6 +4,159 @@ local function buf_dir()
     return d
 end
 
+local function find_command()
+    if vim.fn.executable('rg') == 1 then
+        return { 'rg', '--files', '--color', 'never' }
+    end
+    if vim.fn.executable('fd') == 1 then
+        return { 'fd', '--type', 'f', '--color', 'never' }
+    end
+    if vim.fn.executable('fdfind') == 1 then
+        return { 'fdfind', '--type', 'f', '--color', 'never' }
+    end
+    if vim.fn.executable('find') == 1 and vim.fn.has('win32') == 0 then
+        return { 'find', '.', '-type', 'f' }
+    end
+    if vim.fn.executable('where') == 1 then
+        return { 'where', '/r', '.', '*' }
+    end
+end
+
+local function find_dirs_command()
+    if vim.fn.executable('fd') == 1 then
+        return {
+            'fd',
+            '--type', 'd',
+            '--strip-cwd-prefix',
+            '--exclude', 'node_modules',
+            '--exclude', '.git',
+            '.',
+        }
+    end
+    if vim.fn.executable('fdfind') == 1 then
+        return {
+            'fdfind',
+            '--type', 'd',
+            '--strip-cwd-prefix',
+            '--exclude', 'node_modules',
+            '--exclude', '.git',
+            '.',
+        }
+    end
+    if vim.fn.executable('find') == 1 and vim.fn.has('win32') == 0 then
+        return {
+            'find',
+            '.',
+            '-type', 'd',
+            '!', '-path', '*/node_modules/*',
+            '!', '-path', '*/.git/*',
+        }
+    end
+end
+
+local function file_entry_maker(opts, cwd)
+    return require('telescope.make_entry').gen_from_file(vim.tbl_extend('force', opts or {}, {
+        cwd = cwd,
+    }))
+end
+
+local function parent_aware_file_sorter(opts, strip_prompt)
+    local sorter = require('telescope.config').values.file_sorter(opts)
+    local highlighter = sorter.highlighter
+
+    if highlighter then
+        sorter.highlighter = function(_, prompt, display)
+            return highlighter(sorter, strip_prompt(prompt), display)
+        end
+    end
+
+    return sorter
+end
+
+local function find_files_with_parent()
+    local cwd = buf_dir()
+    local parent = vim.fn.fnamemodify(cwd, ':h')
+    local current_finder_key = cwd
+    local command = find_command()
+    local picker
+
+    local function prompt_targets_parent(prompt)
+        return prompt:sub(1, 2) == '..'
+    end
+
+    local function prompt_without_parent(prompt)
+        if prompt:match('^%.%./') then
+            return prompt:gsub('^%.%./', '', 1)
+        end
+        if prompt:sub(1, 2) == '..' then
+            return prompt:sub(3)
+        end
+        return prompt
+    end
+
+    local function update_picker_cwd(next_cwd)
+        if picker then
+            picker.cwd = next_cwd
+        end
+    end
+
+    local opts = {
+        cwd = cwd,
+        attach_mappings = function(prompt_bufnr)
+            picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+            return true
+        end,
+        on_input_filter_cb = function(prompt)
+            if not command then
+                return { prompt = prompt }
+            end
+
+            local next_finder_key = prompt_targets_parent(prompt) and parent or cwd
+            if next_finder_key == current_finder_key then
+                return {
+                    prompt = prompt_without_parent(prompt),
+                }
+            end
+
+            current_finder_key = next_finder_key
+            update_picker_cwd(next_finder_key)
+
+            return {
+                prompt = prompt_without_parent(prompt),
+                updated_finder = require('telescope.finders').new_oneshot_job(command, {
+                    cwd = next_finder_key,
+                    entry_maker = file_entry_maker(opts, next_finder_key),
+                }),
+            }
+        end,
+    }
+
+    opts.entry_maker = file_entry_maker(opts, cwd)
+    opts.sorter = parent_aware_file_sorter(opts, prompt_without_parent)
+
+    require('telescope.builtin').find_files(opts)
+end
+
+local function find_dirs_here()
+    local pickers = require("telescope.pickers")
+    local finders = require("telescope.finders")
+    local conf = require("telescope.config").values
+
+    local cwd = buf_dir()
+    local command = find_dirs_command()
+    if not command then
+        return
+    end
+
+    pickers.new({}, {
+        prompt_title = "Directories",
+        finder = finders.new_oneshot_job(command, {
+            cwd = cwd,
+        }),
+        sorter = conf.generic_sorter({}),
+    }):find()
+end
+
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
@@ -16,9 +169,8 @@ return {
     end,
     keys = {
         { "<C-f>", function()
-                require('telescope.builtin').find_files({ cwd = buf_dir() })
+                find_files_with_parent()
             end,
-            mode = "n",
             desc = "Telescope find files (here)"
         },
 
@@ -28,6 +180,12 @@ return {
                 })
             end,
             desc = "Telescope grep (args) here"
+        },
+
+        { "<leader>f", function()
+                find_dirs_here()
+            end,
+            desc = "Telescope find directories (here)"
         },
     },
 }
